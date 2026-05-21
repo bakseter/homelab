30d96f7e5f47380498dd840a28f8fc9dd9223fa3
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2026-05-21 17:10:18 +0200

    tofu: cert

--- tofu/authentik.tf
+++ tofu/authentik.tf
@@ -104,12 +104,17 @@ data "authentik_property_mapping_provider_scope" "scopes" {
   ]
 }
 
+data "authentik_certificate_key_pair" "default" {
+  name = "authentik Self-signed Certificate"
+}
+
 resource "authentik_provider_oauth2" "envoy-gateway-sre" {
   name               = "envoy-gateway-sre"
   client_id          = "envoy-gateway-sre"
-  authorization_flow    = data.authentik_flow.default-provider-authorization-explicit-consent.id
-  invalidation_flow     = data.authentik_flow.default-provider-invalidation-flow.id
+  authorization_flow = data.authentik_flow.default-provider-authorization-explicit-consent.id
+  invalidation_flow  = data.authentik_flow.default-provider-invalidation-flow.id
   sub_mode           = "user_username"
+  signing_key        = data.authentik_certificate_key_pair.default.id
   property_mappings  = data.authentik_property_mapping_provider_scope.scopes.ids
   allowed_redirect_uris = [
     {
