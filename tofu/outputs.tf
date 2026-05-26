3e28d2ae609a72a11fb8558dd3f52edf049c33d9
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2026-05-26 15:37:19 +0200

    tofu: outputs

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -2,3 +2,8 @@ output "authentik_argocd_client_secret" {
   value     = authentik_provider_oauth2.argocd.client_secret
   sensitive = true
 }
+
+output "authentik_grafana_client_secret" {
+  value     = authentik_provider_oauth2.grafana.client_secret
+  sensitive = true
+}
