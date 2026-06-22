acd856ef103f5444727b2838b42d4a96808e64b1
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2026-06-22 22:26:02 +0200

    tofu: technitium dns

--- tofu/tailscale.tf
+++ tofu/tailscale.tf
@@ -1,23 +1,35 @@
 provider "tailscale" {
   oauth_client_id     = var.tailscale_oauth_client_id
   oauth_client_secret = var.tailscale_oauth_client_secret
-  tailnet             = "bakseter.github"
+  tailnet             = "bakseter"
 }
 
-resource "tailscale_dns_nameservers" "global" {
-  nameservers = [
-    "100.113.132.29", # infra pihole
-    "1.1.1.1",
-  ]
-}
+locals {
+  technitium_tailscale_ips = toset([
+    "100.85.36.251", # k8s
+    "100.88.208.56", # pi
+  ])
 
-resource "tailscale_dns_split_nameservers" "domains" {
-  for_each = toset([
+  domains = toset([
     "bakseter.net",
     "int.bakseter.net",
     "sre.bakseter.net",
   ])
+}
+
+resource "tailscale_dns_nameservers" "global" {
+  nameservers = setunion(
+    local.technitium_tailscale_ips,
+    toset([
+      "1.1.1.1",
+      "8.8.8.8",
+    ])
+  )
+}
+
+resource "tailscale_dns_split_nameservers" "domains" {
+  for_each = local.domains
 
   domain      = each.key
-  nameservers = ["100.113.132.29"] # infra pihole
+  nameservers = local.technitium_tailscale_ips
 }
