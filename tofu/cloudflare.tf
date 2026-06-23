05d1988f2e3a2785b8c15ce43805c38853cf454c
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2026-06-24 01:29:20 +0200

    tofu: update cloudflare config

--- tofu/cloudflare.tf
+++ tofu/cloudflare.tf
@@ -2,16 +2,14 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-
 locals {
   envoy_gateway = "http://envoy-cloudflared-cloudflared-gateway-7fece151.envoy-gateway-system.svc.cluster.local:80"
   public_domains = [
     "bakseter.no",
-    # # "bakseter.net",
-    # "mandagsmiddag.no",
+    "bakseter.net",
+    "mandagsmiddag.no",
   ]
 }
-
 
 resource "cloudflare_zone" "domain" {
   for_each = toset(local.public_domains)
@@ -23,15 +21,10 @@ resource "cloudflare_zone" "domain" {
   type = "full"
 }
 
-resource "random_id" "homelab" {
-  byte_length = 35
-}
-
 resource "cloudflare_zero_trust_tunnel_cloudflared" "homelab" {
-  account_id    = var.cloudflare_account_id
-  name          = "homelab"
-  config_src    = "cloudflare"
-  tunnel_secret = sensitive(random_id.homelab.b64_std)
+  account_id = var.cloudflare_account_id
+  name       = "homelab"
+  config_src = "cloudflare"
 }
 
 data "cloudflare_zero_trust_tunnel_cloudflared_token" "token" {
@@ -39,21 +32,75 @@ data "cloudflare_zero_trust_tunnel_cloudflared_token" "token" {
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.homelab.id
 }
 
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "config" {
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "homelab" {
   account_id = var.cloudflare_account_id
   tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.homelab.id
   source     = "cloudflare"
 
   config = {
-    ingress = []
+    ingress = [
+      {
+        hostname       = "bakseter.net"
+        origin_request = {}
+        service        = local.envoy_gateway
+      },
+      {
+        hostname       = "authentik.bakseter.net"
+        origin_request = {}
+        service        = local.envoy_gateway
+      },
+      {
+        hostname       = "*.mandagsmiddag.no"
+        origin_request = {}
+        service        = local.envoy_gateway
+      },
+      {
+        hostname       = "mandagsmiddag.no"
+        origin_request = {}
+        service        = local.envoy_gateway
+      },
+      {
+        hostname       = "*.bakseter.no"
+        origin_request = {}
+        service        = local.envoy_gateway
+      },
+      {
+        hostname       = "bakseter.no"
+        origin_request = {}
+        service        = local.envoy_gateway
+      },
+      {
+        service = "http_status:404"
+      },
+    ]
   }
 }
 
-resource "cloudflare_dns_record" "tunnel" {
+resource "cloudflare_dns_record" "tunnel-apex" {
   for_each = toset(local.public_domains)
 
   zone_id = cloudflare_zone.domain[each.key].id
   name    = each.key
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.homelab.id}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "tunnel-wildcard" {
+  for_each = toset(["bakseter.no", "mandagsmiddag.no"])
+
+  zone_id = cloudflare_zone.domain[each.key].id
+  name    = "*.${each.key}"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.homelab.id}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "authentik" {
+  zone_id = cloudflare_zone.domain["bakseter.net"].id
+  name    = "authentik.bakseter.net"
   content = "${cloudflare_zero_trust_tunnel_cloudflared.homelab.id}.cfargotunnel.com"
   type    = "CNAME"
   ttl     = 1
